645c451db32c8dd9dc2aed639fe0f220eb4045e9
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-02-09 12:52:49 +0600

    Navbar and layout section Updated

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+   <nav className='flex justify-center'>
+          <ul className='flex justify-between gap-5'>
+            <li>Home</li>
+            <li>Services</li>
+            <li>About</li>
+          </ul>
+        </nav>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+       <Navbar></Navbar>
+        <main className="h-screen place-content-center">
+          {children}
+        </main>
+        <footer className="text-3xl bg-blue-500 text-center">
+          Awesome nextjs project
+        </footer>
       </body>
     </html>
   );
