27d69561b2106bc81b355b977007bec11be4a1f0
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-02-09 14:44:34 +0600

    About Section Updated

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,12 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
    <nav className='flex justify-center'>
-          <ul className='flex justify-between gap-5'>
-            <li>Home</li>
+          <ul className='flex justify-between gap-10'>
+            <Link href="/">
+             <li>Home</li>
+            </Link>
+
+             <Link href="/services">
             <li>Services</li>
+            </Link>
+
+             <Link href="/about">
             <li>About</li>
+            </Link>
+          
           </ul>
         </nav>
   )
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,27 @@
+"use client"
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function page() {
+    const router = useRouter();
+    const isLOggedIn = false;
+    const handleNavigation = () => {
+        if(isLOggedIn){
+           router.push("/about/address");
+        }
+     else {
+      router.push("/");
+    }
+    }
+   
   return (
-    <div>
+    <div >
       <h2 className='text-4xl text-center'>About Page</h2>
+     <p> <Link href="/about/address">
+      Address Page
+      </Link></p>
+      <button type='button' onClick={handleNavigation}>Address Page</button>
     </div>
   )
 }
