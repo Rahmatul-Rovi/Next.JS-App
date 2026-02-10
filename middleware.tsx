8b27e4585d135758d03fd05d9933e24456601382
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-02-11 00:26:35 +0600

    MiddleWare Section Updated

--- middleware.tsx
+++ middleware.tsx
@@ -3,10 +3,7 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+    console.log(request.nextUrl.pathname)
+  return NextResponse.next();
 }
  
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: '/about/:path*',
-}
